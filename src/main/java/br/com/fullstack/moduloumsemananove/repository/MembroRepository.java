30edd584ce7652e459058424f449dacd3f3cbcc4
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-03-17 23:03:36 -0300

    [M1S09] Ex 6 - Queries UPDATE  - Adicionadas Querys de Update para cada entidade em seu repository equivalente.

--- src/main/java/br/com/fullstack/moduloumsemananove/repository/MembroRepository.java
+++ src/main/java/br/com/fullstack/moduloumsemananove/repository/MembroRepository.java
@@ -2,8 +2,16 @@ package br.com.fullstack.moduloumsemananove.repository;
 
 import br.com.fullstack.moduloumsemananove.model.Membro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface MembroRepository extends JpaRepository<Membro, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Membro m SET m.endereco = COALESCE(:endereco, m.endereco), m.nome = COALESCE(:nome, m.nome), m.telefone = COALESCE(:telefone, m.telefone) WHERE m.id = :membroId")
+    void updateMembro(Long membroId, String endereco, String nome, String telefone);
 }
--- src/main/java/br/com/fullstack/moduloumsemananove/repository/VisitanteRepository.java
+++ src/main/java/br/com/fullstack/moduloumsemananove/repository/VisitanteRepository.java
@@ -2,8 +2,16 @@ package br.com.fullstack.moduloumsemananove.repository;
 
 import br.com.fullstack.moduloumsemananove.model.Visitante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface VisitanteRepository extends JpaRepository<Visitante, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Visitante v SET v.nome = COALESCE(:nome, v.nome), v.telefone = COALESCE(:telefone, v.telefone) WHERE v.id = :visitanteId")
+    void updateVisitante(Long visitanteId, String nome, String telefone);
 }
